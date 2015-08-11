3f583f70c59966d1ad0e97aa7e239f714d2e9575
btz1211 <baote.zheng@gmail.com>
2015-08-10 22:22:58 -0400

    Create JsonKeyConstants.java

---
+++ shared_lib/src/main/java/com/api/constants/JsonKeyConstants.java
@@ -0,0 +1,37 @@
+package com.api.constants;
+
+public class JsonKeyConstants {
+	
+	public static String ID = "id";
+	public static String PATH = "path";
+	public static String NAME = "name";
+	public static String TYPE = "type";
+	public static String MONGO = "mongo";
+	public static String PARAMS = "params";
+	public static String DRIVER = "driver";
+	public static String DRIVERS = "drivers";
+	public static String PRODUCT = "product";
+	public static String COMMAND = "command";
+	public static String VERSION = "version";
+	public static String FETCH_SIZE = "fetch_size";
+	public static String START_INDEX = "start_index";
+	public static String PING_QUERIES = "ping_queries";
+	public static String DATA_SOURCE = "data_source";
+	public static String DATA_METHOD = "data_method";
+	public static String PARAM_NUMBER = "param_number";
+	public static String DATA_SOURCE_ID = "data_source_id";
+	public static String DATA_METHOD_ID = "data_method_id";
+	public static String DATA_SOURCE_TYPE = "data_source_type";
+	public static String DATA_SOURCE_SCHEMA = "data_source_schema";
+	public static String DATA_METHOD_SCHEMA = "data_method_schema";
+	
+	public static String ERROR_CODE = "error_code";
+	public static String ERROR_MESSAGE = "error_message";
+	public static String HTTP_STATUS_CODE = "http_status_code";
+
+
+
+
+
+	
+}
