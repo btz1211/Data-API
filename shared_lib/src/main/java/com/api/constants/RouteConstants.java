9c10ab2e8574009e757da7327dc8eaaf3b395b8b
btz1211 <baote.zheng@gmail.com>
2015-08-10 22:23:24 -0400

    Create RouteConstants.java

---
+++ shared_lib/src/main/java/com/api/constants/RouteConstants.java
@@ -0,0 +1,13 @@
+package com.api.constants;
+
+public class RouteConstants {
+	
+	public static String ROUTE_DATA = "/:version/data_sources/:dataSourceId/data_methods/:dataMethodId";
+	public static String ROUTE_DATA_SOURCES = "/:version/data_sources";
+	public static String ROUTE_DATA_METHODS = "/:version/data_methods";
+	public static String ROUTE_DATA_METHOD="/:version/data_methods/:dataMethodId";
+	public static String ROUTE_DATA_SOURCE = "/:version/data_sources/:dataSourceId";
+	public static String ROUTE_DATA_SOURCE_PING = "/:version/data_sources/:dataSourceId/ping";
+	
+	public static String GUI_HOME = "/home";
+}
